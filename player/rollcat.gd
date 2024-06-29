a6407206af36f42e1bbdfbf517e4a58d5c90b692
LilGrackle <4platedead@gmail.com>
2024-06-28 19:11:46 -0700

    added timer
     bounceTimer to prevent several hitbox overlaps on one "bounce" from counting towards our total. also prevent crazy behavior in tight quarters

--- player/rollcat.gd
+++ player/rollcat.gd
@@ -9,12 +9,14 @@ const JUMPVELOCITY:float = 100
 const FRICTION_ACCEL_BASE:float = 1
 const INITIAL_SPEED:float = 100
 
+const BOUNCE_TIMEOUT_SECONDS:float = 0.25
 
 const SPHERE_MESH_ROTATION_CONST = 0.15
 
 var curr_acceleration_horiz:Vector3
 var last_collider:Object
 var collision_capsule:CollisionShape3D
+var lastbounce_collider:Object
 
 var cSpeed:float
 var velocity2D:Vector2
@@ -25,6 +27,8 @@ var spherePivot:Node3D
 var cameraRot:Node3D
 var cameraHelper:Node3D
 
+var bounceTimer:Timer
+var canBounce:bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -32,7 +36,8 @@ func _ready():
 	#floor
 	velocity = Vector3(0, 0, -1*INITIAL_SPEED)
 	cSpeed = INITIAL_SPEED
-	
+	bounceTimer = get_node("bounceTimer")
+	bounceTimer.wait_time = .05
 	isInAir = true
 	spherePivot = get_node("Pivot/sphere2")
 	cameraRot = get_node("cameraPivot")
@@ -60,40 +65,40 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("jump") and !isInAir:
 		velocity.y = JUMPVELOCITY
-		bounceCounter = 0
 		isInAir=true
 	
-	print(isInAir, velocity.y)
+
 	cSpeed = clamp(cSpeed - (FRICTION_ACCEL_BASE*delta), 0, INITIAL_SPEED)
 	var tmp:float
 	if isInAir:
 		tmp = velocity.y + (VERTICAL_ACCELERATION_BASE*delta)
 	else:
-		velocity.y = 0
-
-		
+		tmp = 0
 	velocity2D = Vector2(velocity.x,velocity.z).normalized()*cSpeed
 	velocity = Vector3(velocity2D.x, tmp, velocity2D.y)
 	
+	#print(isInAir, ", velocity = ", velocity, ", numBounces", bounceCounter )
+	#print(velocity2D.length())
 	var curr_collision = move_and_collide(velocity*delta)
-	
 	
 	if curr_collision:
 		var curr_collider:CollisionObject3D = curr_collision.get_collider()
 		var metaData = curr_collider.get_meta("bouncesUs")
-	
 		if metaData:
-			velocity = velocity.bounce(curr_collision.get_normal())
-			if isInAir and bounceCounter < NUMBOUNCES:
-				#
+			if !canBounce:
+				pass
+			elif isInAir and bounceCounter < NUMBOUNCES and canBounce:
+				velocity = velocity.bounce(curr_collision.get_normal())
 				bounceCounter = bounceCounter + 1
-				#print(bounceCounter)
-			elif bounceCounter == NUMBOUNCES:
+				bounceTimer.start(BOUNCE_TIMEOUT_SECONDS)
+				canBounce = false
+			elif isInAir and bounceCounter == NUMBOUNCES: #stop bouncing
 				isInAir = false
-				velocity = velocity.slide(curr_collision.get_normal())
 				bounceCounter = 0
+			else: #sliding on ground
+				velocity = velocity.slide(curr_collision.get_normal())
 
-	#velocity = Vector3(velocity2D.x, tmp, velocity2D.y)	
-	#print(velocity)
-	
-				
+
+func _on_bounce_timer_timeout():
+	print('yo')
+	canBounce = true # Replace with function body.
